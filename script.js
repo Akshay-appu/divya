75e79bc91e6761310ce7779c0b79b1ca5964f0b5
Akshay-appu <appu03626@gmail.com>
2024-12-11 09:21:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,230 +1,93 @@
-!(function($) {
-  "use strict";
-
-  // Hero typed
-  if ($('.typed').length) {
-    var typed_strings = $(".typed").data('typed-items');
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
-
-  // Smooth scroll for the navigation menu and links with .scrollto classes
-  $(document).on('click', '.nav-menu a, .scrollto', function(e) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      e.preventDefault();
-      var target = $(this.hash);
-      if (target.length) {
-
-        var scrollto = target.offset().top;
-
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
-
-        if ($(this).parents('.nav-menu, .mobile-nav').length) {
-          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-          $(this).closest('li').addClass('active');
-        }
-
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-        }
-        return false;
-      }
-    }
-  });
-
-  // Activate smooth scroll on page load with hash links in the url
-  $(document).ready(function() {
-    if (window.location.hash) {
-      var initial_nav = window.location.hash;
-      if ($(initial_nav).length) {
-        var scrollto = $(initial_nav).offset().top;
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
-      }
-    }
-  });
-
-  $(document).on('click', '.mobile-nav-toggle', function(e) {
-    $('body').toggleClass('mobile-nav-active');
-    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-  });
-
-  $(document).click(function(e) {
-    var container = $(".mobile-nav-toggle");
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
-      if ($('body').hasClass('mobile-nav-active')) {
-        $('body').removeClass('mobile-nav-active');
-        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-      }
-    }
-  });
-
-  // Navigation active state on scroll
-  var nav_sections = $('section');
-  var main_nav = $('.nav-menu, .mobile-nav');
-
-  $(window).on('scroll', function() {
-    var cur_pos = $(this).scrollTop() + 200;
-
-    nav_sections.each(function() {
-      var top = $(this).offset().top,
-        bottom = top + $(this).outerHeight();
-
-      if (cur_pos >= top && cur_pos <= bottom) {
-        if (cur_pos <= bottom) {
-          main_nav.find('li').removeClass('active');
-        }
-        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-      }
-      if (cur_pos < 300) {
-        $(".nav-menu ul:first li:first").addClass('active');
-      }
-    });
-  });
-
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
+$(document).ready(function () {
+  // Scroll down sticky navbar script start
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
     } else {
-      $('.back-to-top').fadeOut('slow');
+      $(".navbar").removeClass("sticky");
     }
-  });
+    // Scroll down sticky navbar script end
 
-  $('.back-to-top').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1500, 'easeInOutExpo');
-    return false;
-  });
+    // Scroll up Button script start
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+    // Scroll up Button script end
 
-  // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
+    // Fade In & Fade Out Elements on Scroll script start
+    $(".fadein").each(function (i) {
+      var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-  // Skills section
-  $('.skills-content').waypoint(function() {
-    $('.progress .progress-bar').each(function() {
-      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+      if (bottom_of_window > bottom_of_element) {
+        $(this).addClass("showme");
+      }
+      if (bottom_of_window < bottom_of_element) {
+        $(this).removeClass("showme");
+      }
     });
-  }, {
-    offset: '80%'
+    // Fade In & Fade Out Elements on Scroll script end
   });
 
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-      aos_init();
-    });
-
-    // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function() {
-      $('.venobox').venobox();
-    });
+  // Scroll up Button script start
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
   });
+  // Scroll up Button script end
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
+  // animation Script for display
+  //const animationScript = ["", "Department of computer Science", "Dr.T.THIMMAIAH INSTITUTE OF TECHNOLOGY"];
+
+  // Typing animation script start
+  new Typed(".typing", {
+    strings: animationScript,
+    typeSpeed: 100,
+    backSpeed: 60,
     loop: true,
+  });
+
+  new Typed(".typing2", {
+    strings: animationScript,
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+  // Typing animation script End
+
+  // toggle menu/navbar script start
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+  // toggle menu/navbar script end
+
+  // owl carousel script start
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
     responsive: {
       0: {
-        items: 1
+        items: 1,
+        nav: false,
       },
-      768: {
-        items: 2
+      600: {
+        items: 2,
+        nav: false,
       },
-      900: {
-        items: 2
-      }
-    }
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
   });
+  // owl carousel script end
+});
 
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
-  // Init AOS
-  function aos_init() {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out-back",
-      once: true
-    });
-  }
-  $(window).on('load', function() {
-    aos_init();
-  });
-
-})(jQuery);
-
-
-/*console print*/
-console.log(
-  `%c                       
-                                ___________________________________________
-                                ___________________________________________
-                                < That's it You can Check my Code Here 😀>
-                                -------------------------------------------
-                                -------------------------------------------
-
- ______             __  __                    __            _______                                                       
- /      \           /  |/  |                  /  |          /       \                                                      
-/$$$$$$  |  ______  $$/ $$ |   __   ______   _$$ |_         $$$$$$$  |  ______   __    __                                  
-$$ \__$$/  /      \ /  |$$ |  /  | /      \ / $$   |        $$ |__$$ | /      \ /  |  /  |                                 
-$$      \  $$$$$$  |$$ |$$ |_/$$/  $$$$$$  |$$$$$$/         $$    $$< /$$$$$$  |$$ |  $$ |                                 
- $$$$$$  | /    $$ |$$ |$$   $$<   /    $$ |  $$ | __       $$$$$$$  |$$ |  $$ |$$ |  $$ |                                 
-/  \__$$ |/$$$$$$$ |$$ |$$$$$$  \ /$$$$$$$ |  $$ |/  |      $$ |  $$ |$$ \__$$ |$$ \__$$ |                                 
-$$    $$/ $$    $$ |$$ |$$ | $$  |$$    $$ |  $$  $$/       $$ |  $$ |$$    $$/ $$    $$ |                                 
- $$$$$$/   $$$$$$$/ $$/ $$/   $$/  $$$$$$$/    $$$$/        $$/   $$/  $$$$$$/   $$$$$$$ |                                 
-                                                                                /  \__$$ |                                 
-                                                                                $$    $$/                                  
-                                                                                 $$$$$$/                                                                                                                                             
-                                                                                                                           
- __       __            __              _______                                 __                                         
-/  |  _  /  |          /  |            /       \                               /  |                                        
-$$ | / \ $$ |  ______  $$ |____        $$$$$$$  |  ______   __     __  ______  $$ |  ______    ______    ______    ______  
-$$ |/$  \$$ | /      \ $$      \       $$ |  $$ | /      \ /  \   /  |/      \ $$ | /      \  /      \  /      \  /      \ 
-$$ /$$$  $$ |/$$$$$$  |$$$$$$$  |      $$ |  $$ |/$$$$$$  |$$  \ /$$//$$$$$$  |$$ |/$$$$$$  |/$$$$$$  |/$$$$$$  |/$$$$$$  |
-$$ $$/$$ $$ |$$    $$ |$$ |  $$ |      $$ |  $$ |$$    $$ | $$  /$$/ $$    $$ |$$ |$$ |  $$ |$$ |  $$ |$$    $$ |$$ |  $$/ 
-$$$$/  $$$$ |$$$$$$$$/ $$ |__$$ |      $$ |__$$ |$$$$$$$$/   $$ $$/  $$$$$$$$/ $$ |$$ \__$$ |$$ |__$$ |$$$$$$$$/ $$ |      
-$$$/    $$$ |$$       |$$    $$/       $$    $$/ $$       |   $$$/   $$       |$$ |$$    $$/ $$    $$/ $$       |$$ |      
-$$/      $$/  $$$$$$$/ $$$$$$$/        $$$$$$$/   $$$$$$$/     $/     $$$$$$$/ $$/  $$$$$$/  $$$$$$$/   $$$$$$$/ $$/       
-                                                                                             $$ |                          
-                                                                                             $$ |                          
-                                                                                             $$/                           
-           
-                                                                                        
-                                                                                        
-                                                                                        
-                                                                               `,
-  "font-family:monospace"
-);
+// Get the current year
+const currentYear = new Date().getFullYear();
+// Set the current year in the span with id "year"
+document.getElementById('year').textContent = currentYear;
